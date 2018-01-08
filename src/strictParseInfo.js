f2018cc0f3cedfb4c1a30922540f5fbd0a3e0fc4
bibli0phile <berapallabi18@gmail.com>
2018-01-08 17:09:22 +0530

    strictParser has been changed and now it will work in caseSensitive way also

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -2,21 +2,29 @@ const Parsed=require("./parsed.js");
 const ParseInfo=require("./parseInfo.js");
 const InvalidKeyError=require("./errors/invalidKeyError.js");
 
-const contains=function(list,key) {
+const contains=function(list,key,state) {
+  let newKey=key;
+  if(!state)
+    newKey = key.toLowerCase();
   return list.find(function(validKey){
-    return key==validKey;
+    let newValidKey = validKey;
+    if(!state){
+      newValidKey = validKey.toLowerCase();
+    }
+    return newKey==newValidKey;
   });
 }
 
-var StrictParseInfo=function(initialParsingFunction,validKeys) {
+var StrictParseInfo=function(initialParsingFunction,validKeys,state) {
   ParseInfo.call(this,initialParsingFunction);
   this.validKeys=validKeys;
+  this.state = state;
 }
 
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  if(!contains(this.validKeys,this.currentKey))
+  if(!contains(this.validKeys,this.currentKey,this.state))
     throw new InvalidKeyError("invalid key",this.currentKey,this.currentPos);
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -13,6 +13,47 @@ describe("strict parser that is case insensitive",function(){
     let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
   });
+  it("should parse when specified keys are in mixed case and actual is not",function(){
+    let kvParser=new StrictParser(["naMe"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["NAME"]="jayanth";
+    let parsed=kvParser.parse("NAME=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified and actual both keys are in mixed case",function(){
+    let kvParser=new StrictParser(["naMe"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["nAmE"]="jayanth";
+    let parsed=kvParser.parse("nAmE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+
+  it("should parse when specified keys are multiple and both keys are in mixed case",function(){
+    let kvParser=new StrictParser(["Age","naMe"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["nAmE"]="jayanth";
+    let parsed=kvParser.parse("nAmE=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are multiple with digit and both keys are in mixed case",function(){
+    let kvParser=new StrictParser(["Age123","naMe456"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["nAmE456"]="jayanth";
+    let parsed=kvParser.parse("nAmE456=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified keys are multiple with underscore_digit and both keys are in mixed case",function(){
+    let kvParser=new StrictParser(["Age_123","naMe_456"],false);
+    // false indicates that case sensitive is false. By default it is true
+    let expected=new Parsed();
+    expected["nAmE_456"]="jayanth";
+    let parsed=kvParser.parse("nAmE_456=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
 });
 
 describe("strict parser that is case sensitive",function(){
@@ -21,6 +62,23 @@ describe("strict parser that is case sensitive",function(){
     // true indicates that parser is case sensitive
     assert.throws(()=>{
       kvParser.parse("NAME=jayanth");
+    })
+  });
+  it("should throw error when specified keys are in mixed case and actual is not",function(){
+    let kvParser=new StrictParser(["naMe"],true);
+    // true indicates that parser is case sensitive
+    assert.throws(()=>{
+      kvParser.parse("NAME=jayanth");
+    })
+  });
+  it("should throw error when multiple specified keys are there in mixed case and actual is not",function(){
+    let kvParser=new StrictParser(["AbcD","naMe"],true);
+    // true indicates that parser is case sensitive
+    assert.throws(()=>{
+      kvParser.parse("NAME=jayanth");
+    })
+    assert.throws(()=>{
+      kvParser.parse("abcd=jayanth");
     })
   });
 });
